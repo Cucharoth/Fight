42b93a8c74f52d30d496a5101a57f7a1e3f8f4a2
Cucharoth <cucharoth@gmail.com>
2024-04-13 18:56:50 -0400

    fix: looks good now + tailwind

--- js/fight.js
+++ js/fight.js
@@ -3,94 +3,93 @@
 //Objeto base para los personajes
 class Character {
     constructor(name, health, damage) {
-      //Atributos
-      this.name = name;
-      this.health = health;
-      this.maxhealth = health;
-      this.damage = damage;
+        //Atributos
+        this.name = name;
+        this.health = health;
+        this.maxhealth = health;
+        this.damage = damage;
     }
     //Verifica si el personaje esta vivo
     isAlive() {
-      return this.health > 0;
+        return this.health > 0;
     }
-  
+
     //Ataca a otro personaje seleccionado
     attack(target) {
-      console.log(`${this.name} deals ${this.damage} DMG to ${target.name}`);
-      target.health -= this.damage;
+        console.log(`${this.name} deals ${this.damage} DMG to ${target.name}`);
+        target.health -= this.damage;
     }
-  
+
     //Retorna la información actual del personaje
     status() {
-      return `${this.name} - HP ${this.health}/${this.maxhealth}`;
+        return `${this.name} - HP ${this.health}/${this.maxhealth}`;
     }
-  }
+}
 
-  function combat(){
-    document.addEventListener('keypress', (e) => {
-        if (e.key == 'n'){
-            if (hero.isAlive()) {
-                hero.attack(enemy);
-                setEnemyCurrentHp((enemy.health / enemy.maxhealth) * 100)
-                console.log(hero.status());
-                console.log(enemy.status());
-                if (!enemy.isAlive()) alert("GAME OVER");
-              } else {
-                console.log(`${hero.name} died!`);
-                alert("GAME OVER")
-              }
-        } else if (e.key == 'x') {
-            if (enemy.isAlive()) {
-                enemy.attack(hero);
-                setHeroCurrentHp((hero.health / hero.maxhealth) * 100)
-                console.log(hero.status());
-                console.log(enemy.status());
-                if (!hero.isAlive()) alert("GAME OVER");
-              } else {
-                console.log(`${enemy.name} died!`);
-                alert("GAME OVER")
-              }
-        }
-    })
-  }
-  
-  //Función para combatir
-  function fight(firstCharacter, secondCharacter) {
-    console.log("Empieza el combate!");
-    console.log(hero.status());
-    console.log(enemy.status());
-    
+function combat() {
+    alert(`COMIENZA EL COMBATE! \n
+    ${hero.status()}, Attack: ${hero.damage}\n
+    ${enemy.status()}, Attack: ${enemy.damage}`);
+    document.addEventListener("keypress", keyPressHandler);
+}
 
-  
-      //Primer personaje ataca si esta vivo
-      
+function keyPressHandler(e) {
+    if (e.key == "x") {
+        hero.attack(enemy);
+        setEnemyCurrentHp((enemy.health / enemy.maxhealth) * 100);
+        console.log(hero.status());
+        console.log(enemy.status());
+    } else if (e.key == "n") {
+        enemy.attack(hero);
+        setHeroCurrentHp((hero.health / hero.maxhealth) * 100);
+        console.log(hero.status());
+        console.log(enemy.status());
+    }
+    updateScreen();
+    statusCheck();
+}
 
-      console.log(`${(hero.health / hero.maxhealth) + 100}`);
-      //setCurrentHp((hero.health / hero.maxhealth) + 100);
-      
-  }
+function statusCheck() {
+    if (!enemy.isAlive()) {
+        document.removeEventListener("keypress", keyPressHandler);
+        document.getElementById("img-enemy").src = "img/defeated-slime.png";
+        setTimeout(() => {
+            if (!alert("GAME OVER \nEnemy died!")) window.location.reload();
+        }, 1000);
+    }
+    if (!hero.isAlive()) {
+        document.removeEventListener("keypress", keyPressHandler);
+        document.getElementById("img-hero").src = "img/Defeated-Knight.jpg";
+        setTimeout(() => {
+            if (!alert("GAME OVER \nHero died!")) window.location.reload();
+        }, 1000);
+    }
+}
 
- 
-
-  function setHeroCurrentHp(hp){
+function setHeroCurrentHp(hp) {
     hpDiv = document.getElementById("heroHp").style.width = hp + "%";
-  }
+}
 
-  function setEnemyCurrentHp(hp){
+function setEnemyCurrentHp(hp) {
     hpDiv = document.getElementById("enemyHp").style.width = hp + "%";
-  }
-  
+}
 
+function updateScreen() {
+    document.getElementById("currentHeroHp").innerHTML = hero.health;
+    document.getElementById("currentEnemyHp").innerHTML = enemy.health;
+    document.getElementById("currentHeroDmg").innerHTML = hero.damage;
+    document.getElementById("currentEnemyDmg").innerHTML = enemy.damage;
+}
 
-  //Creación de personajes
-  const heroHp = Math.floor(Math.random() * 101);
-  const heroDmg = Math.floor(Math.random() * 6);
-  const enemyHp = Math.floor(Math.random() * 101);
-  const enemyDmg = Math.floor(Math.random() * 6);
-  const hero = new Character("Heroe", heroHp, heroDmg);
-  const enemy = new Character("Limo", enemyHp, enemyDmg);
-  
-  //Comenzar combate
-  //fight(hero, enemy);
-  combat();
+//Creación de personajes
+const heroHp = Math.floor(Math.random() * 101 + 10);
+const heroDmg = Math.floor(Math.random() * 6 + 1);
+const enemyHp = Math.floor(Math.random() * 101 + 10);
+const enemyDmg = Math.floor(Math.random() * 6 + 1);
+const hero = new Character("Heroe", heroHp, heroDmg);
+const enemy = new Character("Limo", enemyHp, enemyDmg);
+
+//Comenzar combate
+updateScreen();
+combat();
 
