c9f9ff93081fe8b2d8a3a529dd9b353d42c512e2
Nicolas Arias <101823845+NicolasAriasUfro@users.noreply.github.com>
2024-04-22 12:43:19 -0400

    powerup

--- js/gameManager.js
+++ js/gameManager.js
@@ -3,13 +3,27 @@ function addPowerUp() {
     const coliseum = document.getElementById(idColiseum);
     const heightColiseum = coliseum.offsetHeight;
     const widthColiseum = coliseum.offsetWidth;
+
+
     const powerUp = document.createElement("div");
     powerUp.setAttribute("class", "power-up");
+    //powerUp.setAttribute("class", "power-up-damage");
+    powerUp.style.height = "50px";
+    powerUp.style.width = "50px";
+
+
+    var firepowerimage = document.createElement("img");
+    firepowerimage.setAttribute("src", "../img/powerup/powerup-damage.png");
+    firepowerimage.style.width = "50px";
+    firepowerimage.style.height = "50px";
+
+    powerUp.appendChild(firepowerimage);
+
 
     //get random position
-    let randomX = Math.floor(Math.random() * heightColiseum);
-    let randomY = Math.floor(Math.random() * widthColiseum);
 
+    let randomX = Math.floor(Math.random() * (heightColiseum - 50));
+    let randomY = Math.floor(Math.random() * (widthColiseum  - 50));
 
     powerUp.style.top = randomX + "px";
     powerUp.style.left = randomY + "px";
